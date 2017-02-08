5d5cab9422556d1e56cbc63d2e84582797cd2621
Samsul <sam.arefin003@gmail.com>
2017-02-08 02:44:29 +0100

    lazyload configured

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -4,6 +4,7 @@ import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
   constructor() {
+    this.lazyImages = $(".lazyload");
     this.siteHeader = $(".site-header");
     this.headerTriggerElement = $(".showcase__subtitle");
     this.createHeaderWaypoint();
@@ -11,6 +12,13 @@ class StickyHeader {
     this.headerLinks = $(".primary-nav a");
     this.createPageSectionWaypoints();
     this.smoothScrolling();
+    this.refreshWaypoints();
+  }
+
+  refreshWaypoints() {
+    this.lazyImages.on(function() {
+      Waypoint.refreshAll();
+    });
   }
 
   smoothScrolling() {
